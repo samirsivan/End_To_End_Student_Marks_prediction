758e1fe9268daee77eeae85556a21672e789e036
samirsivan <israksamir353@gmail.com>
2024-09-02 12:57:18 +0600

    Done with logging and exception

---
+++ app.py
@@ -0,0 +1,13 @@
+from src.end_to_end_student_marks_prediction.logger import logging
+from src.end_to_end_student_marks_prediction.exception import CustomException
+import sys
+
+
+
+# if __name__=="__main__":
+#     logging.info("The excution has started")
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Custom Exception")
+#         raise CustomException(e, sys)
